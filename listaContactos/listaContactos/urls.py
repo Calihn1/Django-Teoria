edae586aebdd15a877a7a0386de4a9c2817d56cd
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-29 22:18:48 -0500

    Correcion en from incio

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
-from Inicio.views import anotherView
+from inicio.views import anotherView
 
 urlpatterns = [
     path('', myHomeView, name= 'Pagina de Inicio'),
