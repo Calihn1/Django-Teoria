72af0e4006e6a6d8275fd658c7760501cbdcfe89
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-29 21:38:37 -0500

    Agregando default para evitar que Django lance errores, ahora no es necesario que ingrese valores

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombre  = models.CharField(max_length = 100)
+    nombre    = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad      = models.IntegerField()#(max_digitS = 3)
+    donador   = models.BooleanField(default = False)
