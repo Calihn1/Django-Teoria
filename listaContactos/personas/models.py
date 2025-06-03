ef0be5c65bee2ca1d80035f6377ed198785309ec
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-03 15:47:03 -0500

    Se agrego un diccionario y dara un contexto de mytext y my number, se mostrara en el html

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres  = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
+    nombre    = models.CharField(max_length = 100, null = False, blank = False) #Campo obligatorio
+    apellidos = models.CharField(max_length = 100, null = True, blank = False)
+    edad      = models.IntegerField(null = True, blank = True, default = 18)#(max_digitS = 3)
+    donador   = models.BooleanField(default = True)
