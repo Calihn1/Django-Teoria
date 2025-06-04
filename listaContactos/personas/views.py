4af112f0d5e46b8080fcd8b6a8a1610400574328
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-04 12:52:45 -0500

    Octavo commit: Se envia el objeto enteroy en el mismo html se busca sus valores

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -4,8 +4,12 @@ from .models import Persona
 # Create your views here.
 def personaTestView(request):
     obj = Persona.objects.get( id = 1 )
-    context = {
+    context1 = {
         'nombre' : obj.nombre,
         'edad' : obj.edad,
     }
-    return render(request, 'personas/test.html',context)
+
+    context2 = {
+        'objeto' : obj,
+    }
+    return render(request, 'personas/test.html',context2)
