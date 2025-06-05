a4116d7c5031625a739515b0fafdb71db40af96b
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-05 07:28:51 -0500

    Décimo primer commit: Se agregó un buscador, se le implemento una url

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
-from personas.views import personaTestView
+from personas.views import personaTestView, personaCreateView, searchForHelp
 
 urlpatterns = [
     path('', myHomeView, name= 'Pagina de Inicio'),
     path('another/', anotherView, name='otro'),
     path('persona/', personaTestView, name='otro'),
     path('admin/', admin.site.urls),
+    path('agregar/', personaCreateView, name='createPersona'),
+    path('search/', searchForHelp, name = 'buscar'),
 ]
