bb2fb7c31d7b88e7345b01779d3f0177f3521d26
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-04 23:20:41 -0500

    Décimo commit: Se agregó fields para mostrar el orden del formulario, se creo en templates otro formulario

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombre    = models.CharField(max_length = 100, null = False, blank = False) #Campo obligatorio
+    nombres    = models.CharField(max_length = 100, null = False, blank = False) #Campo obligatorio
     apellidos = models.CharField(max_length = 100, null = True, blank = False)
     edad      = models.IntegerField(null = True, blank = True, default = 18)#(max_digitS = 3)
     donador   = models.BooleanField(default = True)
--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 
 # Create your views here.
 def personaTestView(request):
     obj = Persona.objects.get( id = 1 )
     context1 = {
-        'nombre' : obj.nombre,
+        'nombres' : obj.nombre,
         'edad' : obj.edad,
     }
 
     context2 = {
         'objeto' : obj,
     }
-    return render(request, 'personas/descripcion.html',context2)
+    return render(request, 'personas/descripcion.html', context2)
+
+def personaCreateView(request):
+    form = PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = PersonaForm()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'personas/personasCreate.html', context)
