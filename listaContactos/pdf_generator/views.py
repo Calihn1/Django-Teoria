b74e219dd4fad338b709d83e992437c485f9dce5
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-28 21:58:39 -0500

    Decimo noveno commit: Se agrego la vista que crea el pdf

---
+++ listaContactos/pdf_generator/views.py
@@ -0,0 +1,23 @@
+import datetime
+from django.http import Http404
+from listaContactos.renderers import render_to_pdf
+
+def pdf_view(request):
+    data = {
+        'today':   datetime.date.today(),
+        'amount':  39.99,
+        'customer_name': 'Cooper Mann',
+        'invoice_number': 1233434,
+        'date':    datetime.date.today().strftime('%Y-%m-%d'),
+        'pdf_title':     'Invoice #1233434',
+    }
+    response = render_to_pdf('pdf_generator/invoice.html', data)
+    if response.status_code == 400:
+        raise Http404("Invoice not found")
+
+    filename = f"Invoice_{data['invoice_number']}.pdf"
+    disposition = 'inline; filename=' + filename
+    if request.GET.get('download'):
+        disposition = 'attachment; filename=' + filename
+    response['Content-Disposition'] = disposition
+    return response
