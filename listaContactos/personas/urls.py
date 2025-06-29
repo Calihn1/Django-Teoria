3e5d7148cb3e0aa624bc2fb216e528d113fd2003
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-28 19:33:52 -0500

    Quinto commit: Se creo el formulario para ingresar personas, y su url, y su html

--- listaContactos/personas/urls.py
+++ listaContactos/personas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PersonaListView, PersonaDetailView
+from .views import PersonaListView, PersonaDetailView, PersonaCreateView
 
 urlpatterns = [
     path('', PersonaListView.as_view(), name = 'persona-list'),
-    path('<int:pk>/', PersonaDetailView.as_view(), name = 'persona-detail')
+    path('<int:pk>/', PersonaDetailView.as_view(), name = 'persona-detail'),
+    path('create/', PersonaCreateView.as_view(), name = 'persona-create'),
 ]
--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Persona
 
 #Create your views here
@@ -8,3 +8,9 @@ class PersonaListView(ListView):
 
 class PersonaDetailView(DetailView):
     model = Persona
+
+class PersonaCreateView(CreateView):
+    model = Persona
+    fields = ['nombres', 'apellidos', 'edad', 'donador']
+    template_name = 'personas/persona_form.html'     # o el que prefieras
+    success_url = reverse_lazy('persona-list')
