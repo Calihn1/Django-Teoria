f70f8a503a22ffe837897f3975f3e01c2a98eede
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-28 21:46:07 -0500

    Decimo septimo commit: Se agrego los modelos que peliculas y personajes que tienen relacion de muchos, muchos

--- listaContactos/example/admin.py
+++ listaContactos/example/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Language, Framework
+from .models import Language, Framework, Movie, Character
 
 # Register your models here.
 admin.site.register(Language)
 admin.site.register(Framework)
+admin.site.register(Movie)
+admin.site.register(Character)
